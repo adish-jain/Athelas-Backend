13262240e5b5dca32f69b4e65d550a0b71c5d805
Adish Jain <adish@Adishs-MBP-2.lan>
2022-10-08 15:06:08 -0700

    image rotation working

---
+++ index.js
@@ -0,0 +1,54 @@
+// Entry Point of the API Server
+
+const express = require("express");
+var cors = require("cors");
+const bodyParser = require("body-parser");
+var Jimp = require("jimp");
+
+/* Creates an Express application. 
+   The express() function is a top-level 
+   function exported by the express module.
+*/
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.json());
+
+const rows = [{ id: 1 }, { id: 2 }];
+
+/* To handle the HTTP Methods Body Parser 
+   is used, Generally used to extract the 
+   entire body portion of an incoming 
+   request stream and exposes it on req.body 
+*/
+app.get("/testdata", (req, res, next) => {
+  console.log("TEST DATA :");
+  res.send(rows);
+});
+
+app.post("/uploadImage", async (req, res) => {
+  console.log("uploading image");
+  console.log(req.body);
+  const base64str = req.body.imageFile.replace("data:image/jpeg;base64,", "");
+  console.log(base64str)
+  const buf = Buffer.from(base64str, "base64");
+  console.log(buf)
+  const image = await Jimp.read(buf);
+  console.log(image)
+
+  // rotate Function having a rotation as 90
+  let data = {rotatedImage: ''}
+  image.rotate(90).getBase64(Jimp.MIME_JPEG, function (err, src) {
+    console.log(src);
+    data.rotatedImage = src
+  });
+  res.send(data)
+});
+
+// Require the Routes API
+// Create a Server and run it on the port 3001
+const server = app.listen(3001, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+});
